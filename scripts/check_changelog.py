237fa371f91b8a921807bf8d8c1fce62215af52a
xuebiao.zhao <xuebiao.zhao@tusen.ai>
2023-12-22 14:21:51 +0800

    update check

--- scripts/check_changelog.py
+++ scripts/check_changelog.py
@@ -2,10 +2,12 @@
 import os
 import sys
 
-def process_diff(files):
-    for line in files.strip().split('\n'):
-        status, filename = line.split(maxsplit=1)
-        print(f"File Status: {status}, File Name: {filename}")
+def process_diff(file_path):
+    with open(file_path, 'r') as file:
+        for line in file:
+            status, filename = line.strip().split(maxsplit=1)
+            print(f"File Status: {status}, File Name: {filename}")
+
 
 if __name__ == "__main__":
     changed_files = os.environ.get('CHANGED_FILES', '')
